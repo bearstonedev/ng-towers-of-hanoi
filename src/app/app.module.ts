4258c6f746e1b30093d8c462c60c58a1e834819e
Cam <j.c.doan@outlook.com>
2020-05-09 15:35:21 -0600

    Set up skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { AppComponent } from './app.component'
+import { CoreModule } from './core/core.module'
+import { TowersOfHanoiModule } from './towers-of-hanoi/towers-of-hanoi.module'
 
-import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule,
+    TowersOfHanoiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
